dbcbe904494d1fada73195fe186094a9c832c3c5
Rohan Bhatia <rohanb459@gmail.com>
2022-06-26 18:04:21 +0530

    commit

---
+++ frontend/js/fetch.js
@@ -0,0 +1,74 @@
+
+firebase.auth().onAuthStateChanged(user => {
+    if (user) {
+        console.log(user.uid);
+        console.log(user.email);
+        $(document).ready(function () {
+            const clientID = user.uid;
+            db.collection("users").get().then(function (querySnapshot) {
+                querySnapshot.forEach(function (doc) {
+                    if (doc.id == clientID) {
+                        const data = doc.data();
+                        console.log(data.name);
+                        // console.log($("#name"));
+                        $("#name").text(data.name)
+                        $("#location").text(data.location);
+                        $("#greet").text("Hello "+data.name+", Welcome to your World of Grants!")
+                        $("#Applications").text(data.Applications);
+                        $("#offered").text(data.offered);
+                        $("#awarded").text(data.awarded);
+                        var budg= data.budget;
+                        if(!budg)
+                            budg=33;
+                        console.log(budg);
+                        $("#budget").text(budg+" %");
+                        $("#cons1").text(data.consultant1);
+                        $("#cons2").text(data.consultant2);
+
+                    }
+                });
+            });
+        });
+    }
+})
+
+
+
+$(document).ready(function () {
+    /**
+     * @desc adds onClick to the logout button
+     */
+    function addOnClick() {
+        document.getElementById("logoutButton").onclick = logout;
+    }
+
+    /**
+     * @desc log out current logged in user.
+     */
+    function logout() {
+        firebase.auth().signOut().then(function () {
+            window.location.assign("signIn.html");
+        })
+    }
+
+    document.getElementById("logoutButton").onclick = logout;
+    redirect("./sign.in");
+});
+
+
+
+    function addFile(){
+        var x = document.getElementById("file");
+        var y = document.getElementById("grantInfo");
+        if(x.style.display==="none")
+          {x.style.display = "block";  
+            y.style.display="none";}
+        else
+        {
+            x.style.display= "none";
+            y.style.display="block";
+        }
+    }
+    
+   
+   // document.getElementById("tap").onclick = addFile;
---
+++ frontend/js/subscription.js
@@ -0,0 +1,59 @@
+document.getElementById('Subscriptionform').addEventListener('submit', submitForm);
+
+var email;
+function submitForm(e) {
+    e.preventDefault();
+    email = getTnputValue('subscribedEmail');
+    console.log(email);
+    var today = new Date();
+    var currentdate = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+    saveMessage(email, currentdate);
+}
+
+function getTnputValue(id) {
+    return document.getElementById(id).value;
+}
+
+function sendEmail(subscribedUser, email) {
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            for (let i = 0; i < subscribedUser.length; i++) {
+                db.collection("subscribedUsers").doc(subscribedUser[i]).get().then((doc) => {
+                    Email.send({
+                        Host: "smtp.elasticemail.com",
+                        SecureToken: "24fe7cc3-135a-41e4-9d44-3b24b52bb560",
+                        To: email,
+                        From: "kashish.989392@gmail.com",
+                        Subject: "Thankyou for contacting ResGrant",
+                        Body: "We will reach out to you soon!"
+                    }).then(function () {
+                        console.log("email sent");
+                    }).catch(function (error) {
+                        console.log("Error sending email: " + error);
+                    });
+                });
+            }
+        }
+    });
+
+}
+
+
+function saveMessage(email, currentdate) {
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            db.collection("subscribedUsers").add({
+                email: email,
+                date: currentdate
+            }).then(function () {
+                console.log("New user added to firestore");
+                sendEmail(user.uid, email);
+                var alertstr = "thank you for subscribing";
+                alert(alertstr);
+            }).catch(function (error) {
+                console.log("Error adding new user: " + error);
+            });
+        }
+    });
+}
+
